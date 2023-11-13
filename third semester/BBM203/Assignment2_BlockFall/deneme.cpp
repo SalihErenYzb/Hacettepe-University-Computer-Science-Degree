376ed1eabfb8695fe2e407f99546e07d6a797a86
SalihErenYzb <algosalih@gmail.com>
2023-11-13 14:15:21 +0300

    Reput the correct block text.

--- third semester/BBM203/Assignment2_BlockFall/deneme.cpp
+++ third semester/BBM203/Assignment2_BlockFall/deneme.cpp
@@ -4,10 +4,33 @@
 #include <fstream>
 #include <sstream>
 #include "GameController.h"
+#include <AL/al.h>
+#include <AL/alc.h>
 using namespace std;
 
 
-int main(){
-    cout << "hiii\b\bnaber" << endl;
+int main() {
+    // Initialize OpenAL
+    ALCdevice* device = alcOpenDevice(nullptr);
+    if (!device) {
+        // Handle initialization error
+        return 1;
+    }
 
+    ALCcontext* context = alcCreateContext(device, nullptr);
+    if (!context) {
+        // Handle initialization error
+        alcCloseDevice(device);
+        return 1;
+    }
+
+alcMakeContextCurrent(context);
+
+    // Play the audio file
+    
+    Mix_PlayMusic(music, -1);  // -1 plays the music indefinitely
+
+
+
+    return 0;
 }
